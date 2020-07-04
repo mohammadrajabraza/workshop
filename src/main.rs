5d077b30585d069ed6d72a39e89e292f6913711e
Rajab Raza <mohammadrajabraza@gmail.com>
2020-07-04 16:39:00 +0500

    Spin up an starter hello world example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+// Necessary imports here 
+use actix_web::{App, get, HttpResponse, HttpServer};
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {    
+    // Initializing server
+    HttpServer::new(|| {
+        App::new()
+        // Associating service(s)/route_handler(s)
+         .service(index)
+    })
+    // Binding socket address server will receive requests on
+    .bind("127.0.0.1:5000")?
+    .run()
+    .await
+}
+// ----------------------- Route ------------------------
+// The only route_handler for this example
+#[get("/")]
+async fn index() -> HttpResponse {
+    HttpResponse::Ok().body("Hey! Welcome to the Actix REST API")
 }
