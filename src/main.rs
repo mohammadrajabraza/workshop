468f5d173d37c5e9689a03045e1aff6f172e4436
Rajab Raza <mohammadrajabraza@gmail.com>
2020-07-04 19:26:19 +0500

    added 'create' route-handler for creating a new record

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 // Necessary imports here 
-use actix_web::{App, get, HttpResponse, HttpServer};
+use actix_web::{App, get, post, HttpResponse, HttpServer};
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {    
@@ -17,12 +17,20 @@ async fn main() -> std::io::Result<()> {
 }
 // ----------------------- Route ------------------------
 
+// This route handler will list all the data available
 #[get("/students")]
 async fn find_all() -> HttpResponse {
     HttpResponse::Ok().body("List of students")
 }
 
+// This route handler will list data with specific id
 #[get("/students/{id}")]
 async fn find() -> HttpResponse {
     HttpResponse::Ok().body("Listing student with specific id")
+}
+
+// This route handler will create a new record
+#[post("/students")]
+async fn create() -> HttpResponse {
+    HttpResponse::Ok().body("Creating a new record......")
 }
